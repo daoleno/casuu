f7a83cde0415a7644525a73186281cfe6f745abb
daoleno <daodaoonline@gmail.com>
2020-07-02 13:55:55 +0800

    Add DraggableList

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { Fragment } from "react";
 import CardBox from "./card-box";
+import DraggableList from "./draggable-list";
 import "./styles.css";
+import data from "./data";
 
 function App() {
-  return <CardBox />;
+  return (
+    // <div className="flex items-center justify-center gap-10">
+
+    <DraggableList items={"Lorem ipsum dolor sit".split(" ")} />
+  );
 }
 
 export default App;
